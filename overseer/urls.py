02745eb56c061e0c41594fab7556534b1bd7380a
Brian Bouterse <bmbouter@gmail.com>
2013-10-18 11:10:27 -0400

    Removing /media/ cruft from the urls file

--- overseer/urls.py
+++ overseer/urls.py
@@ -11,11 +11,6 @@ from django.views.generic.base import RedirectView
 import os.path
 
 urlpatterns = patterns('',
-    url(r'^media/(?P<path>.+)?$', 'django.views.static.serve', {
-        'document_root': os.path.join(os.path.dirname(__file__), 'media'),
-        'show_indexes': True
-    }, name='media'),
-
     url(r'^$', 'overseer.views.index', name='index'),
     url(r'^service/(?P<slug>[^/]+)/$', 'overseer.views.service', name='service'),
     url(r'^service/(?P<slug>[^/]+)/last-event/$', 'overseer.views.last_event', name='last_event'),
